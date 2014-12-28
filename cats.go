43af5dd84dfd754d96a1f613c93fa3fb65815b22
ChrisSmith <chris.smith324@gmail.com>
2014-12-28 15:19:27 -0500

    use channels to deliver the images to the callback. kill go routines with a close channel when the app pauses

--- cats.go
+++ cats.go
@@ -3,14 +3,29 @@ package libcats
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
+	"time"
 
 	"encoding/json"
-	"errors"
 
 	"github.com/gregjones/httpcache"
 	"github.com/gregjones/httpcache/diskcache"
 	"github.com/peterbourgon/diskv"
 )
+
+type ImageCallback interface {
+	ImageReceived(image []byte)
+}
+
+func OnStart() {
+	fmt.Printf("onStart()")
+}
+
+func OnStop() {
+	fmt.Printf("onStop()")
+}
 
 var transport *httpcache.Transport
 
@@ -18,9 +33,118 @@ func Init(cachePath string) {
 	transport = newTransportWithDiskCache(cachePath)
 }
 
-func GetCats() (*RedditUrlCollection, error) {
+type CallbackToken struct {
+	done          chan struct{}
+	imageCallback ImageCallback
+	wg            sync.WaitGroup
+}
 
-	resp, err := transport.Client().Get("http://www.reddit.com/r/aww.json")
+func (token *CallbackToken) Close() {
+	close(token.done)
+	token.wg.Wait()
+	fmt.Printf("callback closed.\n")
+}
+
+func CreateImageCallback(callback ImageCallback) *CallbackToken {
+	imageCallback := &CallbackToken{
+		imageCallback: callback,
+		done:          make(chan struct{}),
+		wg:            sync.WaitGroup{},
+	}
+
+	go startTimer(imageCallback)
+
+	return imageCallback
+}
+
+// Buffers 4 images in memory ready to be consumed by
+// the timer when it ticks
+func startTimer(token *CallbackToken) {
+	fmt.Printf("starting callback timer\n")
+
+	urls := produceCatUrls(token.done)
+
+	var images = make(chan []byte, 4)
+	defer close(images)
+
+	// need to wait for produces to exit before
+	// closing the images channel
+	token.wg.Add(4)
+
+	for i := 0; i < 4; i++ {
+		go consumeUrl(urls, images, token.done, &token.wg)
+	}
+
+	c := time.Tick(5 * time.Second)
+
+	var img []byte
+	for {
+		select {
+		case <-token.done:
+			token.wg.Wait()
+			log.Printf("receive on done channel")
+			return
+		case <-c:
+			{
+				select {
+				case <-token.done:
+					token.wg.Wait()
+					log.Printf("receive on done channel")
+					return
+				case img = <-images:
+					fmt.Printf("sending callback\n")
+					token.imageCallback.ImageReceived(img)
+				}
+			}
+		}
+	}
+}
+
+func consumeUrl(urls chan string, images chan []byte, done chan struct{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for url := range urls {
+		fmt.Printf("downloading %s\n", url)
+		if bytes, err := DownloadBytes(url); err == nil {
+			select {
+			case <-done:
+				return
+			case images <- bytes:
+			}
+		}
+	}
+}
+
+func produceCatUrls(done chan struct{}) chan string {
+	var urls = make(chan string)
+
+	go func() {
+		defer close(urls)
+		response, err := getCats()
+
+		if err != nil {
+			log.Printf("failed to produceCatUrls. " + err.Error())
+			return
+		}
+
+		for _, v := range response.Data.Children {
+			if v.Kind == "t3" && v.Data.Url != "" {
+				select {
+				case <-done:
+					return
+				case urls <- v.Data.Url:
+				}
+
+			}
+		}
+	}()
+
+	return urls
+}
+
+func getCats() (*redditResponseDto, error) {
+
+	resp, err := getClient().Get("http://www.reddit.com/r/aww.json")
 	if err != nil {
 		return nil, err
 	}
@@ -33,27 +157,7 @@ func GetCats() (*RedditUrlCollection, error) {
 		return nil, err
 	}
 
-	urls := make([]string, 0)
-	for _, v := range response.Data.Children {
-		if v.Kind == "t3" && v.Data.Url != "" {
-			urls = append(urls, v.Data.Url)
-		}
-	}
-
-	return &RedditUrlCollection{
-		urls: urls,
-	}, nil
-}
-
-func (dto *RedditUrlCollection) GetUrl(index int) (string, error) {
-	if index < len(dto.urls) && index >= 0 {
-		return dto.urls[index], nil
-	}
-	return "", errors.New("invalid index")
-}
-
-type RedditUrlCollection struct {
-	urls []string
+	return &response, nil
 }
 
 type redditResponseDto struct {
@@ -75,9 +179,9 @@ type redditPostDto struct {
 	Url string
 }
 
-func DownloadCat(url string) ([]byte, error) {
+func DownloadBytes(url string) ([]byte, error) {
 
-	resp, err := transport.Client().Get(url)
+	resp, err := getClient().Get(url)
 	if err != nil {
 		return nil, err
 	}
@@ -103,4 +207,10 @@ func newTransportWithDiskCache(basePath string) *httpcache.Transport {
 	c := diskcache.NewWithDiskv(d)
 
 	return httpcache.NewTransport(c)
+}
+
+func getClient() *http.Client {
+	c := transport.Client()
+	//c.Timeout = time.Duration(30 * time.Second) //TODO Client Transport of type *httpcache.Transport doesn't support CanelRequest; Timeout not supported
+	return c
 }
--- cats_test.go
+++ cats_test.go
@@ -2,21 +2,58 @@ package libcats
 
 import (
 	"io/ioutil"
+	"log"
 	"os"
+	"runtime"
+	"runtime/pprof"
 	"testing"
+	"time"
 )
 
-func TestReverse(t *testing.T) {
+func TestCreateImageCallback(t *testing.T) {
 	dir, err := ioutil.TempDir(os.TempDir(), "libcats")
 	defer os.RemoveAll(dir)
 	Init(dir)
-	urls, err := GetCats()
 
 	if err != nil {
 		t.Errorf("%s", err.Error())
 	}
 
-	if len(urls.urls) == 0 {
-		t.Errorf("no urls", "")
+	printHeap()
+
+	callback := TestImageCallback{}
+	token := CreateImageCallback(callback)
+
+	// pprof.Lookup("goroutine").WriteTo(os.Stdout, 1)
+
+	toEnd := time.After(5 * time.Second)
+	toPrint := time.Tick(1 * time.Second)
+	for {
+		select {
+		case <-toEnd:
+
+			token.Close()
+
+			// All go routines should have stopped
+			pprof.Lookup("goroutine").WriteTo(os.Stdout, 1)
+			return
+		case <-toPrint:
+			printHeap()
+		}
 	}
+}
+
+func printHeap() {
+	runtime.GC()
+	memstats := new(runtime.MemStats)
+	runtime.ReadMemStats(memstats)
+	log.Printf("memstats: bytes = %d footprint = %d", memstats.HeapAlloc, memstats.Sys)
+}
+
+func (callback TestImageCallback) ImageReceived(image []byte) {
+	callback.imagesReceived += 1
+}
+
+type TestImageCallback struct {
+	imagesReceived int
 }
